5fd3beaa6e700e8db85ca2f6c5c40a3eb4ffef7c
AIra <AIra.work213@gmail.com>
2025-10-19 21:22:26 +0300

    Исправлен промпт для гигачата

--- server/models/gigachatModel.py
+++ server/models/gigachatModel.py
@@ -22,14 +22,21 @@ class GigaModel:
         return res
     
     def get_questions(self, text):
-        response = self.giga.chat(f"""Тебе дан текст: {text}. Твоя задача - составить 10 вопросов по этому тексту и ответы к ним, пусть на вопросы можно будет ответить 1-2 предложениями и сами вопросы небольшие. Никакого текста кроме вопросов и ответов.
-                                  Ответ представь в формате JSON:""" + "\n{questions: [вопрос 1, вопрос 2, вопрос 3, вопрос 4, вопрос 5, вопрос 6, вопрос 7, вопрос 8, вопрос 9, вопрос 10], answers: [ответ 1, ответ 2, ответ 3, ответ 4, ответ 5, ответ 6, ответ 7, ответ 8, ответ 9, ответ 10]}"
-                                  )
+        print(f"Получение вопросов для текста: {text}")
+        response = self.giga.chat(
+            "Тебе дан текст:\n" +
+            f"{text}\n\n" +
+            "Составь 10 коротких вопросов по тексту и краткие ответы на них.\n" +
+            "Верни строго валидный JSON без какого-либо дополнительного текста, без markdown, без комментариев.\n" +
+            'Строгий формат:\n' +
+            '{"questions": ["вопрос 1", "вопрос 2", "...", "вопрос 10"], ' +
+            '"answers": ["ответ 1", "ответ 2", "...", "ответ 10"]}'
+            )
         start_index = response.choices[0].message.content.find('{')
         end_index = response.choices[0].message.content.rfind('}') + 1
         res = response.choices[0].message.content[start_index:end_index]
-        res = json.loads(res)
-        return res
+        print(res)
+        return json.loads(res)
 
 if __name__ == '__main__':
     giga = GigaModel()
