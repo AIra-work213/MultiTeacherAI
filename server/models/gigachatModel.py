ea6cfc5ca6236f3f8393e4bba73cd5bb71fc76bb
AIra <AIra.work213@gmail.com>
2025-10-19 18:57:35 +0300

    Добавлено получение вопросов по тексту

--- server/models/gigachatModel.py
+++ server/models/gigachatModel.py
@@ -1,5 +1,5 @@
 from gigachat import GigaChat
-
+import json
 
 class GigaModel:
     def __init__(self):
@@ -11,7 +11,7 @@ class GigaModel:
     
 
     def question(self, question, text):
-        response = self.giga.chat(f"Тебе дан текст: {text}. Ответь на вопрос: {question}. Не более 300 слов")
+        response = self.giga.chat(f"Тебе дан текст: {text}. Ответь на вопрос: {question}. Отвечай на вопрос только в том случае, если ты уверен что этот вопрос по тому контексту, который тебе предоставили, иначе ответь: Я не могу ответить на текущий вопрос. Не более 300 слов")
         res = response.choices[0].message.content
         return res
 
@@ -19,6 +19,16 @@ class GigaModel:
     def get_topic(self, text):
         response = self.giga.chat(f"Тебе дан текст: {text}. Твоя задача - написать только одно предложение, которое будет являться темой этого текста. Не нужно ничего кроме этой темы.")
         res = response.choices[0].message.content
+        return res
+    
+    def get_questions(self, text):
+        response = self.giga.chat(f"""Тебе дан текст: {text}. Твоя задача - составить 10 вопросов по этому тексту и ответы к ним, пусть на вопросы можно будет ответить 1-2 предложениями и сами вопросы небольшие. Никакого текста кроме вопросов и ответов.
+                                  Ответ представь в формате JSON:""" + "\n{questions: [вопрос 1, вопрос 2, вопрос 3, вопрос 4, вопрос 5, вопрос 6, вопрос 7, вопрос 8, вопрос 9, вопрос 10], answers: [ответ 1, ответ 2, ответ 3, ответ 4, ответ 5, ответ 6, ответ 7, ответ 8, ответ 9, ответ 10]}"
+                                  )
+        start_index = response.choices[0].message.content.find('{')
+        end_index = response.choices[0].message.content.rfind('}') + 1
+        res = response.choices[0].message.content[start_index:end_index]
+        res = json.loads(res)
         return res
 
 if __name__ == '__main__':
